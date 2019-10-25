b4e2c2d22e3a1559e4840761e0e7ac979310d00c
juunnn <junaedi.fahmi15081@student.unsika.ac.id>
2019-10-25 17:48:47 +0700

    added new files

---
+++ precache-manifest.3a9c73b2d41c72e0d55a6a3fc916b310.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f3e63857f985c620e873da6c8cbc4b",
+    "url": "/juunnn.github.io/index.html"
+  },
+  {
+    "revision": "fb4876f1bb8839b40350",
+    "url": "/juunnn.github.io/static/css/main.c685064e.chunk.css"
+  },
+  {
+    "revision": "91f7178b766cc576187d",
+    "url": "/juunnn.github.io/static/js/2.5669c958.chunk.js"
+  },
+  {
+    "revision": "fb4876f1bb8839b40350",
+    "url": "/juunnn.github.io/static/js/main.0a9831da.chunk.js"
+  },
+  {
+    "revision": "850b9affac4f001e58f6",
+    "url": "/juunnn.github.io/static/js/runtime~main.aaff3caa.js"
+  },
+  {
+    "revision": "8601bf7fe9735480e019b03d337af2f2",
+    "url": "/juunnn.github.io/static/media/Telegram.8601bf7f.jpg"
+  },
+  {
+    "revision": "c9e84820d16bb8441fec522ad32e945a",
+    "url": "/juunnn.github.io/static/media/WhatsApp.c9e84820.png"
+  },
+  {
+    "revision": "d74e6036cb5b6e80d87b0a3fc77176c0",
+    "url": "/juunnn.github.io/static/media/gmail.d74e6036.png"
+  },
+  {
+    "revision": "9f1eed6fd531c1d0e7c1a14d2699eaed",
+    "url": "/juunnn.github.io/static/media/kkn1.9f1eed6f.jpg"
+  },
+  {
+    "revision": "23c9022fa91ddbf45d9a093962b28369",
+    "url": "/juunnn.github.io/static/media/rtik.23c9022f.jpg"
+  }
+]);
